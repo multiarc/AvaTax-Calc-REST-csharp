dd1670d609ac9e6182153ba6f569a8539f4af96b
Anya Stettler <anya.stettler@avalara.com>
2013-06-11 13:27:34 -0700

    whitespace cleanup

--- DocumentLoader.cs
+++ DocumentLoader.cs
@@ -20,23 +20,23 @@ namespace AvaTaxCalcREST
             //
 
             // Set the tax document properties - Required unless noted as Optional
-            getTaxRequest.CompanyCode = "SDK";                        // R: Company Code from the accounts Admin Console
+            getTaxRequest.CompanyCode = "SDK";                              // R: Company Code from the accounts Admin Console
             getTaxRequest.Client = "AvaTaxCalcRESTCsharp Sample";
             DateTime docDate = DateTime.Today;
-            getTaxRequest.DocCode = "SampleDoc: " + docDate.ToString();  			// R: Invoice or document tracking number - Must be unique
+            getTaxRequest.DocCode = "SampleDoc: " + docDate.ToString();  	// R: Invoice or document tracking number - Must be unique
             getTaxRequest.DocType = DocType.SalesInvoice;                	// R: Typically SalesOrder,SalesInvoice, ReturnInvoice
-            getTaxRequest.DocDate = docDate.ToString("yyyy-MM-dd");                            // R:  sets reporting date and default tax date
+            getTaxRequest.DocDate = docDate.ToString("yyyy-MM-dd");         // R:  sets reporting date and default tax date
             getTaxRequest.CustomerCode = "TaxSvcTest";                   	// R: String - Customer Tracking number or Exemption Customer Code
             getTaxRequest.DetailLevel = DetailLevel.Tax;             		// R: Chose Summary, Document, Line or Tax - varying levels of results detail 
             getTaxRequest.Commit = false;                           		// O: Default is "false" - Set to "true" to commit the Document
-            //getTaxRequest.CustomerUsageType="G";						// O: Send for tax exempt transactions only.
-            //getTaxRequest.ExemptionNo="12334";						// O: Send for tax exempt transactions only.
-            //getTaxRequest.Discount=0;									// O: Send for document-level discounts only.
-            getTaxRequest.PurchaseOrderNo = "PO 23423";					// O: Specifies the purchase order number associated with the transaction. This value can be used to track single-use exemption certficates.
+            //getTaxRequest.CustomerUsageType="G";						    // O: Send for tax exempt transactions only.
+            //getTaxRequest.ExemptionNo="12334";						    // O: Send for tax exempt transactions only.
+            //getTaxRequest.Discount=0;									    // O: Send for document-level discounts only.
+            getTaxRequest.PurchaseOrderNo = "PO 23423";					    // O: Specifies the purchase order number associated with the transaction. This value can be used to track single-use exemption certficates.
             getTaxRequest.ReferenceCode = "";								// O: This is a reportable value that does not affect tax calculation.
-            getTaxRequest.PosLaneCode = "";								// O: This is a reportable value that does not affect tax calculation.
-            //getTaxRequest.TaxOverride=new TaxOverrideDef();			// O: Allows the TaxDate =or other values) to be overridden for tax calculation. Situational only.				
-            //getTaxRequest.BusinessIdentificationNo="";				// O: Specified VAT ID of customer for international/VAT calculations and reporting.
+            getTaxRequest.PosLaneCode = "";								    // O: This is a reportable value that does not affect tax calculation.
+            //getTaxRequest.TaxOverride=new TaxOverrideDef();			    // O: Allows the TaxDate =or other values) to be overridden for tax calculation. Situational only.				
+            //getTaxRequest.BusinessIdentificationNo="";				    // O: Specified VAT ID of customer for international/VAT calculations and reporting.
 
 
             //	          Begin Address Section
@@ -84,31 +84,31 @@ namespace AvaTaxCalcREST
 
             // Add invoice lines
 
-            Line line1 = new Line();                               // New instance of a line                                
-            line1.LineNo = "101";                              // R: string - line Number of invoice - must be unique.
-            line1.ItemCode = "Item001";                   // R: string - SKU or short name of Item
-            line1.Qty = 1;               // R: decimal - The number of items -- Qty of product sold. Does not function as a mulitplier for Amount
-            line1.Amount = 1000;  // R: decimal - the "NET" amount -- Amount should be the 'extended' or 'net' amount
-            line1.CustomerUsageType = "";           // O: string - AKA Entity Use Code - Typically A - L =G = Reseller)
-            line1.Description = "ITEM1";           // O: string - Description or category of item sold.
-            line1.TaxCode = "";                   // O: string - Pass standard, custom or Pro-Tax code
-            //              Can be NULL to default to tangible personal property =P0000000)
-            line1.OriginCode = "Origin";                      	// R: Value representing the Origin Address
-            line1.DestinationCode = "Dest";                 	  	// R: Value representing the Destination Address
+            Line line1 = new Line();                                        // New instance of a line                                
+            line1.LineNo = "101";                                           // R: string - line Number of invoice - must be unique.
+            line1.ItemCode = "Item001";                                     // R: string - SKU or short name of Item
+            line1.Qty = 1;                                                  // R: decimal - The number of items -- Qty of product sold. Does not function as a mulitplier for Amount
+            line1.Amount = 1000;                                            // R: decimal - the "NET" amount -- Amount should be the 'extended' or 'net' amount
+            line1.CustomerUsageType = "";                                   // O: string - AKA Entity Use Code - Typically A - L =G = Reseller)
+            line1.Description = "ITEM1";                                    // O: string - Description or category of item sold.
+            line1.TaxCode = "";                                             // O: string - Pass standard, custom or Pro-Tax code
+                                                                            //              Can be NULL to default to tangible personal property =P0000000)
+            line1.OriginCode = "Origin";                      	            // R: Value representing the Origin Address
+            line1.DestinationCode = "Dest";                 	  	        // R: Value representing the Destination Address
 
             //Line 2 - Shipping/Freight line - See property descriptions above
-            Line line2 = new Line();                                   // New instance of a line
-            line2.LineNo = "102";                                   // R: string - SKU or short name of Item
-            line2.ItemCode = "Shipping";                       // R: string - SKU or short name of Item
-            line2.Description = "Shipping- Freight Charges";  // O: string - Description or category of item sold.
-            line2.Qty = 1;                   // R: decimal - The number of items -- Qty of product sold. Does not function as a mulitplier for Amount
-            line2.Amount = 10;        // R: decimal - the "NET" amount -- Amount should be the 'extended' or 'net' amount
-            line2.TaxCode = "FR";                       // O: string - Pass standard, custom or Pro-Tax code FR020100
-            line2.OriginCode = "Origin";                      	// R: Value representing the Origin Address
-            line2.DestinationCode = "Dest";                 	  	// R: Value representing the Destination Address
+            Line line2 = new Line();                                        // New instance of a line
+            line2.LineNo = "102";                                           // R: string - SKU or short name of Item
+            line2.ItemCode = "Shipping";                                    // R: string - SKU or short name of Item
+            line2.Description = "Shipping- Freight Charges";                // O: string - Description or category of item sold.
+            line2.Qty = 1;                                                  // R: decimal - The number of items -- Qty of product sold. Does not function as a mulitplier for Amount
+            line2.Amount = 10;                                              // R: decimal - the "NET" amount -- Amount should be the 'extended' or 'net' amount
+            line2.TaxCode = "FR";                                           // O: string - Pass standard, custom or Pro-Tax code FR020100
+            line2.OriginCode = "Origin";                      	            // R: Value representing the Origin Address
+            line2.DestinationCode = "Dest";                 	  	        // R: Value representing the Destination Address
 
             Line[] lines = { line1, line2 };
-            getTaxRequest.Lines = lines;            // Sets array of lines
+            getTaxRequest.Lines = lines;                                    // Sets array of lines
 
 
             return getTaxRequest;
